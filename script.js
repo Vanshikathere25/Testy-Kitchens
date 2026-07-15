22a96a51f97d345dd32a41a460844a61e4543029
Vanshikathere25 <vanshikathere9@gmail.com>
2026-07-15 22:53:30 +0530

    Add cart item count functionality
    Initialize count variable and update cart count on item addition.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let count = 0;
+
 function addCart() {
     alert("Item Added To Cart Successfully!");
+    
+    // कार्ट काउंट को 1 बढ़ाएं
+    count = count + 1;
+    
+    // स्क्रीन पर दिख रहे नंबर को अपडेट करें
+    document.getElementById("cart-count").innerText = count;
 }
